1436d8860e2ce7fae5fd07a7370cc4dcd822449a
TodorMi <46191374+TodorMi@users.noreply.github.com>
2021-01-13 16:39:21 +0200

    Exporting database

--- web/modules/custom/quote_block/src/Plugin/Block/QuoteBlock.php
+++ web/modules/custom/quote_block/src/Plugin/Block/QuoteBlock.php
@@ -27,14 +27,14 @@ class QuoteBlock extends BlockBase {
 
   private function getRandQuote() {
     $quotes = [
-      '<h3>"Start where you are. Use what you have. Do what you can. <i>-Anne Frank</i>"</h3>',
-      '<h3>"The secret of success is to do the common things uncommonly well. <i>-John D. Rockefeller</i>"</h3>',
-      '<h3>"Strive for progress, not perfection. <i>-Unknown</i>"</h3>',
-      '<h3>"There are two kinds of people in this world: those who want to get things done and those who don’t want to make mistakes. <i>-John Maxwell</i>"</h3>',
-      '<h3>"The secret to getting ahead is getting started. <i>-Unknown</i>"</h3>',
-      '<h3>"There is no substitute for hard work. <i>-Thomas Edison</i>"</h3>',
-      '<h3>"The only place where success comes before work is in the dictionary. <i>-Vidal Sassoon</i>"</h3>',
-      '<h3>"Don’t let what you cannot do interfere with what you can do. <i>-John Wooden</i>"</h3>',
+      '<h4>"Start where you are. Use what you have. Do what you can. <i>-Anne Frank</i>"</h4>',
+      '<h4>"The secret of success is to do the common things uncommonly well. <i>-John D. Rockefeller</i>"</h4>',
+      '<h4>"Strive for progress, not perfection. <i>-Unknown</i>"</h4>',
+      '<h4>"There are two kinds of people in this world: those who want to get things done and those who don’t want to make mistakes. <i>-John Maxwell</i>"</h4>',
+      '<h4>"The secret to getting ahead is getting started. <i>-Unknown</i>"</h4>',
+      '<h4>"There is no substitute for hard work. <i>-Thomas Edison</i>"</h4>',
+      '<h4>"The only place where success comes before work is in the dictionary. <i>-Vidal Sassoon</i>"</h4>',
+      '<h4>"Don’t let what you cannot do interfere with what you can do. <i>-John Wooden</i>"</h4>',
     ];
     return $quotes[array_rand($quotes)];
   }
